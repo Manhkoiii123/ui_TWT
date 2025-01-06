88b737c8c7f7184faa448b28171f9765dcf9c96f
manhtranduc0202 <manhtranduc0202@gmail.com>
2025-01-06 15:49:01 +0700

    fix useSearchParams

--- app/templates/edit/page.tsx
+++ app/templates/edit/page.tsx
@@ -1,10 +1,12 @@
 import CreateTemplateComponent from "@/app/(components)/create-email/CreateTemplateComponent";
-import React from "react";
+import React, { Suspense } from "react";
 
 const EditTemplatePage = () => {
   return (
     <div className="bg-white p-4 shadow-sm">
-      <CreateTemplateComponent />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CreateTemplateComponent />
+      </Suspense>
     </div>
   );
 };
