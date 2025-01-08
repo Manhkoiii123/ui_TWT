75e5d076b4d5d5d8defc0a7243a9cf2993528f44
manhtranduc0202 <manhtranduc0202@gmail.com>
2025-01-08 13:09:48 +0700

    integrate api upload image in grapesjs

--- api/upload/uploadApi.ts
+++ api/upload/uploadApi.ts
@@ -8,9 +8,18 @@ export const uploadApi = {
     const res: UploadImage = await apiClient.post("/upload-file", body);
     return res;
   },
+  removeImage: async (filePath: string) => {
+    const res = await apiClient.delete("remove-file", { data: { filePath } });
+    return res;
+  },
 };
 export const useMutationUploadImage = () => {
   return useMutation({
     mutationFn: (body: FormData) => uploadApi.uploadImage(body),
+  });
+};
+export const useMutationRemoveImage = () => {
+  return useMutation({
+    mutationFn: (filePath: string) => uploadApi.removeImage(filePath),
   });
 };
