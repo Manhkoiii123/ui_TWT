13e8f41f318ee96d04f57b49c387ac3220080a4e
manhtranduc0202 <manhtranduc0202@gmail.com>
2025-01-15 16:05:29 +0700

    build ui email setting

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 const CustomInput = () => {
   return (
-    <main className="p-24">
-      <label className="flex flex-col-reverse relative focus group">
-        <input required className="border-2 border-black px-4 py-3 leading-9" />
-
-        <span className="absolute text-xl transform -translate-y-2 left-4 transition leading-10 group-focus-within:-translate-y-16">
-          Email address *
-        </span>
-      </label>
-    </main>
+    <label className="relative cursor-pointer">
+      <input
+        type="text"
+        placeholder="Input"
+        className="h-10 w-96 px-6  text-red-500  border-gray-300 border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
+      />
+      <span className="bg-white text-gray-500 text-opacity-80 z-10 absolute left-2 top-2 px-1 transition duration-200 input-text">
+        Input
+      </span>
+    </label>
   );
 };
 
